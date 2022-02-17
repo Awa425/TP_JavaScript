5b3d53307e5dc0e895df2ceb1fd8c233bbe6e744
Awa Diop <diopawa192@gmail.com>
2022-02-17 18:20:35 +0000

    commit 1

--- tp1/script.js
+++ tp1/script.js
@@ -2,8 +2,6 @@
 const btnAdd = document.getElementById('btnAdd')
 const container = document.getElementById('container')
 
-// const text = document.getElementById('icon') 
-
 
 
 
@@ -13,24 +11,29 @@ function createCard() {
     const card = document.createElement('div');
     card.setAttribute('class', 'card')
     card.setAttribute('id', 'id_'+i)
-
+    console.log(card)
     card.innerHTML = `  <div class="header">
                             <button class="icon iconDelete" onclick='Delete(${i})'><i class="fa fa-trash" style='font-size:24px'></i></button>
-                            <button class="icon iconAdd"><i class='fas fa-edit' style='font-size:24px'></i></button>
+                            <button class="icon iconAdd" onclick='edit(${i})'><i class='fas fa-edit' style='font-size:24px'></i></button>
                         </div>
                         <div class="center">
-                             <textarea name="" class="" cols="auto"  ></textarea>    
+                             <textarea name="" class="txt" cols="auto" id="txt_${i}"></textarea>    
                         </div>`
     container.appendChild(card) 
     const iconDelete = document.querySelector('.iconDelete')
     i++
 }   
 function Delete(i) {
-    let Delete = document.getElementById('id_'+i)
-    Delete.remove()
-    // console.log(Delete)
+    let elt = document.getElementById('id_'+i)
+    elt.remove()
+    console.log(elt)
 }
-
+function edit(li) {
+    let edit = document.getElementById('txt_'+li) 
+     console.log(edit)
+    edit.toggleAttribute('disabled') 
+    edit.focus() 
+}
 
 
 //EVENTS
