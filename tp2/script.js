39fae5a4521eccb5bd31c7c1d7c5400456240ef3
Awa Diop <diopawa192@gmail.com>
2022-02-18 18:32:27 +0000

    commit tp2

--- tp2/script.js
+++ tp2/script.js
@@ -4,39 +4,56 @@ const btnGauche = document.querySelector('.btnGauche')
 const bloc3 = document.querySelector('.bloc3')
 
 
+
+
+
 let myList = [
     {name: 'Mon premier', activated: 0},
     {name: 'Mon deuxieme', activated: 0},
     {name: 'Mon trosieme', activated: 0},
     {name: 'Mon quatrieme', activated: 0}
 ]
+let myList3 = [
+    
+]
+
 console.log(myList)
-
-
-
-
-
-
-
 
 //Fonction
 function creerTable() {
    bloc1.innerHTML =''; 
     const table = document.createElement('table')
+    const tab3 = document.createElement('table')
+
+
     for (let i = 0; i < 4; i++) {
         const tr = document.createElement('tr')
         const p = document.createElement('p')
         p.innerText = myList[i].name
+        // console.log(p)
 
         //events TR 
-        tr.addEventListener('click', function () {
-            myList[i].activated=!myList[i].activated
-            // if (myList[i].activated) {
+            tr.addEventListener('click', function () {
+                creerTable()
+                myList[i].activated=!myList[i].activated
+                // let x=0
+                if (myList[i].activated) {
+                    btnGauche.addEventListener('click', ()=>{
+                        tab3.innerHTML=myList[i].name
+                            // tab3.appendChild(p)
+                        myList.splice(0);
+                        // p.remove()
+                        // x++
+                        // creerTable()
+                        console.log(myList[i].name)
+                        //  creerTable()
+                    });    
+                }
+                creerTable()
                 
-            // }
-            creerTable()
 
-        });
+            });
+          
         tr.style.backgroundColor= myList[i].activated ? "blue" : "rgb(211, 207, 207)";
 
         //events btn
@@ -54,6 +71,10 @@ function creerTable() {
         tr.appendChild(p)
         table.appendChild(tr)
         bloc1.appendChild(table)
+
+        //ajout dans les parent bloc3
+        bloc3.appendChild(tab3)
+
       
     }
 }
